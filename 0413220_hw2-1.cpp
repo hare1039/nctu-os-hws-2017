db1f9367a0115a13618b6199e6860da35100b23b
hare1039 <hare39393@gmail.com>
2017-11-02 00:45:54 +0800

    Add pinfo header file

--- 0413220_hw2-1.cpp
+++ 0413220_hw2-1.cpp
@@ -3,51 +3,11 @@
 #include <vector>
 #include <map>
 #include <cstdio>
-
-struct info
-{
-	int id, start, burst, finished, at_tick_start, at_tick_end;
-	info():id(0),
-		   start(0),
-		   burst(0),
-		   finished(0),
-		   at_tick_start(0),
-		   at_tick_end(0){}
-	info(int id, int start, int burst): id(id),
-										start(start),
-										burst(burst),
-										finished(0),
-										at_tick_start(0),
-										at_tick_end(0){}
-
-	bool is_done(int at_tick)
-	{
-		if(finished == burst)
-			at_tick_end = at_tick;
-		return finished >= burst;
-	}
-
-	void use_cpu(int at_tick)
-	{
-		if(finished == 0)
-			at_tick_start = at_tick;
-		finished++;
-	}
-	int waiting() {return at_tick_end - start - burst;}
-	int turnaround() {return at_tick_end - start;}
-
-	void show()
-	{
-		std::cout << "id                " << id << "\n"
-				  << "want to start at: " << start << "\n"
-				  << "really start at:  " << at_tick_start << "\n"
-				  << "really end at:    " << at_tick_end << "\n"
-				  << "burst:            " << burst << std::endl;
-	}
-};
+#include "pinfo.hpp"
 
 int main(int argc, char *argv[])
 {
+	using prpr::info;
 	std::fstream input("Q1.txt", std::ios_base::in);
 	if (not input.is_open())
 		return 1;
--- 0413220_hw2-3.cpp
+++ 0413220_hw2-3.cpp
@@ -4,56 +4,11 @@
 #include <map>
 #include <cstdio>
 #include <algorithm>
-
-struct info
-{
-	int id, start, burst, finished, at_tick_start, at_tick_end;
-	info():id(0),
-		   start(0),
-		   burst(0),
-		   finished(0),
-		   at_tick_start(0),
-		   at_tick_end(0){}
-	info(int id, int start, int burst): id(id),
-										start(start),
-										burst(burst),
-										finished(0),
-										at_tick_start(0),
-										at_tick_end(0){}
-
-	bool is_done(int at_tick)
-	{
-		if(finished == burst)
-			at_tick_end = at_tick;
-		return finished >= burst;
-	}
-
-	void use_cpu(int at_tick)
-	{
-		if(finished == 0)
-			at_tick_start = at_tick;
-		finished++;
-	}
-	int waiting() {return at_tick_end - start - burst;}
-	int turnaround() {return at_tick_end - start;}
-
-	void show()
-	{
-		std::cout << "id                " << id << "\n"
-				  << "want to start at: " << start << "\n"
-				  << "really start at:  " << at_tick_start << "\n"
-				  << "really end at:    " << at_tick_end << "\n"
-				  << "burst:            " << burst << std::endl << std::endl;
-	}
-
-	bool operator < (info const &b) const
-	{
-		return this->burst < b.burst;
-	}
-};
+#include "pinfo.hpp"
 
 int main(int argc, char *argv[])
 {
+	using prpr::info;
 	std::fstream input("Q3.txt", std::ios_base::in);
 	if (not input.is_open())
 		return 1;
